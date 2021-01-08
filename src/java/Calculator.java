7eaeee92c4b8fa36f8c52e2ac2fac07ae1987e66
Martinnes <martin@hemmingsens.dk>
2021-01-08 10:55:43 +0100

    Add functionality for Calculator

---
+++ src/java/Calculator.java
@@ -0,0 +1,13 @@
+package java;
+
+public class Calculator {
+    public static void main(String[] args) throws Exception {
+        if(args.length!=2)
+            throw new Exception("There should be two input arguments used with the calculator");
+
+        String arg1 = args[0];
+        String arg2 = args[1];
+
+        System.out.println(arg1 + "+" + arg2 + "=" + Integer.parseInt(arg1)+Integer.parseInt(arg2));
+    }
+}
