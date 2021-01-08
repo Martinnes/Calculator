a81ae79692345ce2525dd3e454f95f4db66952ff
Martinnes <martin@hemmingsens.dk>
2021-01-08 11:51:32 +0100

    updated calculator

--- src/java/Calculator.java
+++ src/java/Calculator.java
@@ -1,15 +1,9 @@
 package java;
 
 public class Calculator {
-    public static void main(String[] args) throws Exception {
-        if(args.length!=2)
-            throw new Exception("There should be two input arguments used with the calculator");
-
-        String arg1 = args[0];
-        String arg2 = args[1];
-
-
-
-        System.out.println(arg1 + "+" + arg2 + "=" + Integer.parseInt(arg1)+Integer.parseInt(arg2));
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println(a + b);
     }
 }
